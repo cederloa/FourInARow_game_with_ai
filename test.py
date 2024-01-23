d8905af7fd3a6a014a600e079dbd1e0f742e2f2c
Antti Cederlöf <antti.cederlof@tuni.fi>
2024-01-23 22:38:44 +0200

    Testing with varying models

--- test.py
+++ test.py
@@ -4,19 +4,22 @@ import itertools
 import torch
 from models.dqn import Dqn
 
+# NOTE:
+# The model does not know how to avoid losing. Find a way to give -1 rewards
+
 
 def main():
-    RLmodel = torch.load("models/savedModels/firstModel.pt")
-    initModel = torch.load("models/savedModels/initModel.pt")
+    RLmodel1 = torch.load("models/savedModels/CNN_5x5_3x3/90step_100ep_model.pt")
+    RLmodel2 = torch.load("models/savedModels/RewardsFixed/90step_100ep_model.pt")
 
     gui = Gui_4iar()
-    p1 = aiPlayer("P1", model=RLmodel)
-    p2 = aiPlayer("P2", model="random")
+    p1 = aiPlayer("P1", model=RLmodel1)
+    p2 = aiPlayer("P2", model=RLmodel2)
 
     while gui.getGame().getResults() == False:
         p1.set_state(gui.getGame().getGameBoard())
         p2.set_state(gui.getGame().getGameBoard())
-        if gui.getGame().getInturn() == "P1":
+        if gui.getGame().getInturn().get_id() == "P1":
             gui.drop(p1.choose_action())
         else:
             gui.drop(p2.choose_action())
